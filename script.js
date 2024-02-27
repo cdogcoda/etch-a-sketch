a426641cdb343ec95f2196b499bbb0a22a748cd6
CDog <cdog97456@gmail.com>
2024-02-26 21:20:46 -0800

    Add container div to contain grids to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+// TOP's pseudocode steps
+
+// Create a 16x16 grid first of square divs using JS (the space should be 960px wide)
+// -> These sqaures should go inside a container div that can then go directly in the html
+// -> This means that the container div will be a flex container to the squares (flex items)
+// Add event listeners to every square such that when the mouse hovers over it, it changes the background color of the square
+// Create a popup that allows users to enter a number for which a new grid with those dimensions can be generated
+// -> Gives user opportunities to create new grids and drawings
+
